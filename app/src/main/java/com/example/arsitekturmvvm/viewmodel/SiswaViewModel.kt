d05f169cc39edd7100ed94b43bb8c6cf5de98ab7
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 09:27:25 +0700

    feat: membuat function untuk insert dan update dengan parameter ls bertipe data string dan parameter bisa diubah2

--- app/src/main/java/com/example/arsitekturmvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/arsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -2,10 +2,17 @@ package com.example.arsitekturmvvm.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.arsitekturmvvm.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel() {
-    private val _statusUI = MutableStateflow(value = Siswa())
+    private val _statusUI = MutableStateFlow(value = Siswa())
 
-    val statusUI : StateFlow<Siswa> = _statusUI.asStateflow()
+    val statusUI : StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>){
+
+    }
 }
