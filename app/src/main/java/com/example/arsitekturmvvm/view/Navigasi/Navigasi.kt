9a16b043c5bcd0beed51d209110006f373902117
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 16:59:32 +0700

    feat: membuat function SiswaApp dengan deklarasi variable  viewModel untuk tempat data dan logika serta parameter navcontroller untuk mengelola alur navigasi antar tampilan

---
+++ app/src/main/java/com/example/arsitekturmvvm/view/Navigasi/Navigasi.kt
@@ -0,0 +1,24 @@
+package com.example.arsitekturmvvm.view.Navigasi
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvvm.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavController = rememberNavController()
+){
+
+}
