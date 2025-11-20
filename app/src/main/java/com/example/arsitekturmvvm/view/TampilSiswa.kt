da7201a9ccdb1181992ea9b9b459a1cd1c84653d
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 16:52:51 +0700

    feat: menambahkan button untuk back ke halaman sebelumnya

--- app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
@@ -3,8 +3,10 @@ package com.example.arsitekturmvvm.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -58,6 +60,12 @@ fun TampilSiswa(
                     Divider(thickness = dimensionResource(R.dimen.thickness_divider))
                 }
                 Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackButtonClicked
+                ) {
+                    Text(text = stringResource(R.string.back))
+                }
             }
         }
     }
