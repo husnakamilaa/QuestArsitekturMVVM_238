46e39c083c7757032f4454a889f8a6510fadbc7c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 17:13:30 +0700

    feat: deklarasi NavHost dan mengintegrasikan State Flow dari View Model untuk databinding

--- app/src/main/java/com/example/arsitekturmvvm/view/Navigasi/Navigasi.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/Navigasi/Navigasi.kt
@@ -1,10 +1,15 @@
 package com.example.arsitekturmvvm.view.Navigasi
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.arsitekturmvvm.viewmodel.SiswaViewModel
 
@@ -18,7 +23,17 @@ enum class Navigasi {
 fun SiswaApp(
     modifier: Modifier,
     viewModel: SiswaViewModel = viewModel(),
-    navController: NavController = rememberNavController()
+    navController: NavHostController = rememberNavController()
 ){
+    Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
 
+            modifier = Modifier.padding(isiRuang)
+        ) {
+
+        }
+    }
 }
