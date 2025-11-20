7ba5062da9d8fdf5bed8de28292b3014544393c0
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 12:08:24 +0700

    feat: membuat layout column pada isiRuang

--- app/src/main/java/com/example/arsitekturmvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/FormSiswa.kt
@@ -1,5 +1,8 @@
 package com.example.arsitekturmvvm.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -36,5 +40,13 @@ fun FormSiswa(
                     containerColor = colorResource(R.color.purple_200))
             )
         }
-    )
+    ){ isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+        }
+    }
 }
